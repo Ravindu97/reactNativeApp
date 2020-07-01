1a4f7fed5f7dc8d53837413354edfe633934c732
RavinduFernando <f.ravindu@gmail.com>
2020-07-01 19:20:10 +0530

    Cleared all the setting up bugs in the react native app

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World updated</Text>
+      <Text>Hello react native App</Text>
       <StatusBar style="auto" />
     </View>
   );
